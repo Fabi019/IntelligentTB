ae68f47b95f06d9b9c7fe936eab38d9a8db68be2
Fabi019 <fabi019@gmx.de>
2023-11-27 20:17:44 +0100

    Small refactor

--- IntelligentTB/stdafx.h
+++ IntelligentTB/stdafx.h
@@ -7,7 +7,6 @@ processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #include <SDKDDKVer.h>
 
 #pragma comment(lib, "kernel32.lib")
-#pragma comment(lib, "msvcrt.lib")
 #pragma comment(lib, "user32.lib")
 #pragma comment(lib, "Advapi32.lib")
 #pragma comment(lib, "shell32.lib")
